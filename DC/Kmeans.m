54b8dbe214968ba865c8a30b334600e67ed8ecb4
marko0630 <76942461+marko0630@users.noreply.github.com>
2021-01-09 15:50:21 +0100

    Update Kmeans.m

--- DC/Kmeans.m
+++ DC/Kmeans.m
@@ -1,4 +1,4 @@
-function  [] = Kmeans()
+function  [img_g] = Kmeans()
 
 [ime_dat, put] = uigetfile('*.png' );          %odabir slike..odaberi izoštrenu sliku
 img = (imread([put '\' ime_dat]));
@@ -6,11 +6,14 @@ img = (imread([put '\' ime_dat]));
 
 L = imsegkmeans(img, 2);         %kmeans segmentacija
 B = labeloverlay(img, L);
+B = im2gray(B);
+t = graythresh(B);
+BW = imbinarize(B,t);
 
 figure('Name','kMeans segmentacija','NumberTitle','off');
 subplot(1,2,1),imshow(img), title('originalna slika');
-subplot(1,2,2), imshow(B), title('segmentirana slika');
+subplot(1,2,2), imshow(BW), title('segmentirana slika');
 
-imwrite(B, 'segmentirana_slika_Kmeans.jpg');
+imwrite(BW, 'segmentirana_slika_Kmeans.jpg');
 
 end
